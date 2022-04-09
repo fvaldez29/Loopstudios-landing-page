f9bc15bc8e1be69a30c8d0ebb593247cf422abb0
fvaldez29 <fvaldez29@hotmail.com>
2022-04-09 18:26:19 -0400

    Js fixed

--- script/main.js
+++ script/main.js
@@ -1,8 +1,9 @@
 const hamburger = document.querySelector('#icon');
-
+const closeBtn = document.querySelector('#btn-close');
 const navBar = document.querySelector('.navbar');
 
 const navbarToggle = () => navBar.classList.toggle('active');
 
 hamburger.addEventListener('click', navbarToggle);
 
+closeBtn.addEventListener('click', navbarToggle);
